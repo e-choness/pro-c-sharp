60843c04e4aa1172ad74c30b8a87c7d865606971
Echo Yin <echo_ybl@hotmail.com>
2023-08-18 21:06:57 -0400

    Learn param keyword and 2d array expression

---
+++ SomeCSharpPractice/KeywordParam.cs
@@ -0,0 +1,14 @@
+﻿namespace SomeCSharpPractice;
+
+public static class KeywordParam
+{
+    public static double Checkout(params double[] prices)
+    {
+        double total = 0;
+        foreach (double price in prices)
+        {
+            total += price;
+        }
+        return total;
+    }
+}
---
+++ SomeCSharpPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SomeCSharpPractice;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        // This is a param keyword practices, it's like C++ ...
+        Console.WriteLine(KeywordParam.Checkout(1.0));
+        Console.WriteLine(KeywordParam.Checkout(2.0, 3.0));
+        Console.WriteLine(KeywordParam.Checkout(5.0, 6.0, 7.0));
+        Console.WriteLine(KeywordParam.Checkout(new double[]{9.0, 10.0, 11.0, 12.0, 13.0}));
+        
+        // Output A 2D array values
+        TwoDimenArray twoDimenArray = new TwoDimenArray();
+        
+        
+    }
+    
+}
