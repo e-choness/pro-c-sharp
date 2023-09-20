440e66d63b3f9a9391ebb51749226610e4361854
Echo Yin <echo_ybl@hotmail.com>
2023-09-19 23:21:43 -0400

    Get to know more about delegates and events

---
+++ SomeCSharpPractice/DelegateEvents.cs
@@ -0,0 +1,17 @@
+﻿namespace SomeCSharpPractice;
+
+public class DelegateEvents
+{
+    public delegate void Callback(string message);
+
+    private static void DelegateMethod(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static void DisplayCallbackHandlerResult()
+    {
+        Callback handler = DelegateMethod;
+        handler("Hello delegates!");
+    }
+}
