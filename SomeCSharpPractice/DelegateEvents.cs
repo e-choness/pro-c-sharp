09eb36bceaffd041e8c0d4abae2a313d072811a6
Echo Yin <echo_ybl@hotmail.com>
2023-09-19 23:51:26 -0400

    Aggregate delegates

--- SomeCSharpPractice/DelegateEvents.cs
+++ SomeCSharpPractice/DelegateEvents.cs
@@ -1,6 +1,6 @@
 ﻿namespace SomeCSharpPractice;
 
-public class DelegateEvents
+public static class DelegateEvents
 {
     public delegate void Callback(string message);
 
@@ -11,7 +11,40 @@ public class DelegateEvents
 
     public static void DisplayCallbackHandlerResult()
     {
+        MethodClass methodClass = new();
+        Callback callback1 = methodClass.Method1;
+        Callback callback2 = methodClass.Method2;
         Callback handler = DelegateMethod;
-        handler("Hello delegates!");
+
+        Callback allDelegates = callback1 + callback2 + handler;
+        allDelegates("Hello delegates!");
+        MethodWithCallback(3,4, allDelegates);
+        
+        Console.WriteLine("Removing callback 1.");
+        allDelegates -= callback1;
+        allDelegates("Goodbye callback 1");
+        Console.WriteLine("Removing callback 2.");
+        allDelegates -= callback2;
+        allDelegates("Goodbye callback 2");
+        allDelegates -= handler;
+        // alldelegates("Goodbye handler.");
+    }
+
+    public static void MethodWithCallback(int param1, int param2, Callback callback)
+    {
+        callback($"The sum of two number is: {(param1+param2).ToString()}");
+    }
+}
+
+public class MethodClass
+{
+    public void Method1(string message)
+    {
+        Console.WriteLine($"{message} is from method 1.");
+    }
+
+    public void Method2(string message)
+    {
+        Console.WriteLine($"{message} is from method 2.");
     }
 }
