7745a4df80c5cc9af3469a63edd1f02963fa1d56
Echo Yin <echo_ybl@hotmail.com>
2023-10-07 22:02:25 -0400

    Minor stuff

---
+++ SomeCSharpPractice/AccessModifiers.cs
@@ -0,0 +1,15 @@
+﻿namespace SomeCSharpPractice;
+
+public class AccessModifiers
+{
+    public static void DisplayResults()
+    {
+        
+    }
+    
+}
+
+public class BaseClas
+{
+    internal static int m_base = 0;
+}
---
+++ SomeCSharpPractice/DependencyInversion.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+
+namespace SomeCSharpPractice;
+
+public class DependencyInversion
+{
+    public void DisplayResult()
+    {
+        
+       
+    }
+}
+
+public abstract class Beverage
+{
+    protected string? Description;
+    protected double Price;
+
+    protected Beverage(string? description, double price)
+    {
+        Description = description;
+        Price = price;
+    }
+
+    public abstract string? Cost();
+
+    public virtual string? GetDescription()
+    {
+        return Description;
+    }
+}
+
+public class HouseBlend : Beverage
+{
+    
+    public HouseBlend(string? description, double price) : base(description, price)
+    {
+        Description = description;
+        Price = price;
+    }
+
+    public override string? Cost()
+    {
+        return Description;
+    }
+}
+
+public class DarkRoast : Beverage
+{
+    public DarkRoast(string? description, double price) : base(description, price)
+    {
+        Description = description;
+        Price = price;
+    }
+
+    public override string? Cost()
+    {
+        return Description;
+    }
+}
+
+public interface IAddOn
+{
+    
+}
