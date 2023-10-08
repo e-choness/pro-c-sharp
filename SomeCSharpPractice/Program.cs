4fdf2495bd1428c8494cdff945d791daa7bbe88b
Echo Yin <echo_ybl@hotmail.com>
2023-10-08 00:07:07 -0400

    Vector 2 division operation overload

--- SomeCSharpPractice/Program.cs
+++ SomeCSharpPractice/Program.cs
@@ -5,32 +5,34 @@ static class Program
     static void Main(string[] args)
     {
         // This is a param keyword practices, it's like C++ ...
-        Console.WriteLine(KeywordParam.Checkout(1.0));
-        Console.WriteLine(KeywordParam.Checkout(2.0, 3.0));
-        Console.WriteLine(KeywordParam.Checkout(5.0, 6.0, 7.0));
-        Console.WriteLine(KeywordParam.Checkout(new double[]{9.0, 10.0, 11.0, 12.0, 13.0}));
+  //       Console.WriteLine(KeywordParam.Checkout(1.0));
+  //       Console.WriteLine(KeywordParam.Checkout(2.0, 3.0));
+  //       Console.WriteLine(KeywordParam.Checkout(5.0, 6.0, 7.0));
+  //       Console.WriteLine(KeywordParam.Checkout(new double[]{9.0, 10.0, 11.0, 12.0, 13.0}));
+  //       
+  //       // Output A 2D array values
+  //       TwoDimenArray twoDimenArray = new TwoDimenArray();
+  //       
+  //       // Inheritance in C#, abstract class indicates missing components and it is to be completed via inheritance classes 
+  //       AbstractClasses.ShowResult1();
+  //        
+  //       // ToString() can also be overridden.
+  //       ToStringOverride.DisplayResult();
+  //
+		// // Generics are like Templates in C++, put <T> to describe a particular datatype and using the same logic.
+		// GenericArray.DisplayArrays();
+  //       
+  //       // Threading uses System.Threading each thread can run at the same time and Thread can take lambda expressions.
+  //       // MultiThreading.RunThreads();
+  //       
+  //       DelegateEvents.DisplayCallbackHandlerResult();
+  //       PublisherSubscriber.DisplayResult();
+  //       
+  //       BaseDerivative.DisplayResult();
+  //       
+  //       EventInterfaces.DisplayResult();
         
-        // Output A 2D array values
-        TwoDimenArray twoDimenArray = new TwoDimenArray();
-        
-        // Inheritance in C#, abstract class indicates missing components and it is to be completed via inheritance classes 
-        AbstractClasses.ShowResult1();
-         
-        // ToString() can also be overridden.
-        ToStringOverride.DisplayResult();
-
-		// Generics are like Templates in C++, put <T> to describe a particular datatype and using the same logic.
-		GenericArray.DisplayArrays();
-        
-        // Threading uses System.Threading each thread can run at the same time and Thread can take lambda expressions.
-        // MultiThreading.RunThreads();
-        
-        DelegateEvents.DisplayCallbackHandlerResult();
-        PublisherSubscriber.DisplayResult();
-        
-        BaseDerivative.DisplayResult();
-        
-        EventInterfaces.DisplayResult();
+        OperatorOverload.ShowResult();
     }
     
 }
