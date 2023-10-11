34e4ec7f4a6cfafae6e2077bf62bea6874b2e86a
Echo Yin <echo_ybl@hotmail.com>
2023-10-11 00:22:21 -0400

    Experiment on non-keys

---
+++ SomeCSharpPractice/DictionaryRemove.cs
@@ -0,0 +1,16 @@
+﻿namespace SomeCSharpPractice;
+
+public class DictionaryRemove
+{
+
+    public static void ShowResult()
+    {
+        var dict = new Dictionary<string, string>()
+        {
+            { "12345", "I'm unique" }
+        };
+
+        Console.WriteLine(dict.Remove("1111"));
+    }
+}
+
