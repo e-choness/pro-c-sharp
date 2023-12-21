3a173ab4b08886c1e34b02ca3c08ab80e0c2a71c
Echo Yin <echo_ybl@hotmail.com>
2023-12-20 21:58:30 -0500

    Making sells operations

--- DependencyInversion/Program.cs
+++ DependencyInversion/Program.cs
@@ -2,14 +2,33 @@
 using SolidLibrary.Models;
 using SolidLibrary.Services;
 
-Console.WriteLine("Yeap");
-
+// Create a game called Starfield
 Game game = (Game)Factory.CreateGame();
 game.Id = 0;
 game.Title = "Starfield";
 game.Subtitle = "";
-game.Price = 0;
+game.Price = 70.0f;
 
-Retailer retailer = (Retailer)Factory.CreateRetailer();
-retailer.Store = "Steam";
-retailer.Game = game;
+// Create a retailer called Steam, which is sending emails for reporting revenue
+Retailer emailRetailer = (Retailer)Factory.CreateRetailer();
+emailRetailer.Store = "Steam";
+emailRetailer.Game = game;
+emailRetailer.Revenue = 0;
+emailRetailer.IsSoldOut = false;
+
+// Create a retailer called Xbox, which is sending texts for reporting revenue
+Retailer textRetailer = (Retailer)Factory.CreateAnotherRetaier();
+textRetailer.Store = "Xbox";
+textRetailer.Game = game;
+textRetailer.Revenue = 0;
+textRetailer.IsSoldOut = false;
+
+// Steam's sales operations
+emailRetailer.SellGame(game.Price);
+emailRetailer.SellGame(game.Price * 0.8);
+emailRetailer.CompleteSale();
+
+// Xbox's sales operations
+textRetailer.SellGame(game.Price * 1.1);
+textRetailer.SellGame(game.Price);
+textRetailer.CompleteSale();
