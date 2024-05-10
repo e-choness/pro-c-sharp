ddc67655d8771b1eb21346c0483b502507c5c29c
Echo Yin <echo_ybl@hotmail.com>
2024-05-10 09:47:27 -0600

    Basic string operations

---
+++ ProSharp/CommonTypeSystem/BasicStrings.cs
@@ -0,0 +1,26 @@
+﻿namespace ProSharp.CommonTypeSystem;
+
+public class BasicStrings
+{
+    public static void Functionalities()
+    {
+        Console.WriteLine("=> Basic String Functionality");
+        
+        const string firstName = "Freddy";
+        Console.WriteLine("Value of firstName: {0}", firstName);
+        Console.WriteLine("firstName has {0} characters.", firstName.Length);
+        Console.WriteLine("firstName in uppercase {0}", firstName.ToUpper());
+        Console.WriteLine("firstName in lowercase {0}", firstName.ToLower());
+        Console.WriteLine("firstName contains the letter y: {0}", firstName.Contains('y'));
+        Console.WriteLine("New first name: {0}", firstName.Replace("dy", ""));
+    }
+
+    public static void ShowConcatenation()
+    {
+        Console.WriteLine("=> String concatenation");
+        var s1 = "Programming the ";
+        var s2 = "PsychoDrill (PTP)";
+        var s3 = s1 + s2;
+        Console.WriteLine(s3);
+    }
+}
--- ProSharp/Program.cs
+++ ProSharp/Program.cs
@@ -9,8 +9,14 @@ StringFormatters.FormatNumericalData();
 Objects.FunctionalitiesShowcase();
 NumericalData.DataTypeFunctionality();
 SystemChar.CharFunctionality();
+
 StringParser.ParseFromStrings();
 StringParser.ParseFromStringWithTryParse();
 
 TimeSpans.UseDatesAndTimes();
 TimeSpans.UseDateTimeOnly();
+
+BigInts.UseBigInteger();
+
+BasicStrings.Functionalities();
+BasicStrings.ShowConcatenation();
