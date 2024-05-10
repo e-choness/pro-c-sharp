e2359c74a6195a9a2a5f4053e65baea44ca0cc9f
Echo Yin <echo_ybl@hotmail.com>
2024-05-10 00:09:45 -0600

    Date and time type functionalities

--- ProSharp/Program.cs
+++ ProSharp/Program.cs
@@ -1,11 +1,16 @@
 ﻿using ProSharp.CommonTypeSystem;
+using ProSharp.DateTimes;
 using ProSharp.IO;
 
 EnvironmentArgsProvider.ShowEnvironmentDetails();
 // BasicIO.BasicConsole();
+
 StringFormatters.FormatNumericalData();
 Objects.FunctionalitiesShowcase();
 NumericalData.DataTypeFunctionality();
 SystemChar.CharFunctionality();
 StringParser.ParseFromStrings();
 StringParser.ParseFromStringWithTryParse();
+
+TimeSpans.UseDatesAndTimes();
+TimeSpans.UseDateTimeOnly();
