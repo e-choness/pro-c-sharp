0e0eb8eed23a787b38589605dead788b308e23bb
Echo Yin <echo_ybl@hotmail.com>
2024-05-11 22:59:03 -0600

    Add a partial class with additional property

---
+++ ProSharp/PartialClasses/Item.Core.cs
@@ -0,0 +1,15 @@
+﻿namespace ProSharp.PartialClasses;
+
+public partial class Item(int id, string name, DateTime creationDate)
+{
+    public int Id { get; init; } = id;
+    public string Name { get; set; } = name;
+    public DateTime CreationDate { get; init; } = creationDate;
+
+    public Item() : this(0, "Default", DateTime.Now) { }
+
+    public string GetInfo()
+    {
+        return $"The item info - ID: {Id} Name - {Name} Creation Date: {CreationDate}";
+    }
+}
---
+++ ProSharp/PartialClasses/ItemDistributor.cs
@@ -0,0 +1,12 @@
+﻿namespace ProSharp.PartialClasses;
+
+public class ItemDistributor
+{
+    public static void Distribute()
+    {
+        Console.WriteLine("=> Distribute an item with partial class");
+        var item = new Item();
+        Console.WriteLine(item.GetInfo());
+        Console.WriteLine(item.GetInfoWithDescription());
+    }
+}
