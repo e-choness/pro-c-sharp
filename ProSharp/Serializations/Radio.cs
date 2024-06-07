6a72d6307f725ec08c5a8c3de608a2013eef9449
Echo Yin <echo_ybl@hotmail.com>
2024-06-06 21:32:24 -0600

    Setup for serialization showcase

---
+++ ProSharp/Serializations/Radio.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace ProSharp.Serializations;
+
+public class Radio
+{
+    public bool HasTweeters { get; set; }
+    public bool HasSubWoofers { get; set; }
+    public List<double> StationPresets { get; set; }
+    public string RadioId { get; set; } = "XF-552RR6";
+    public override string ToString()
+    {
+        var presets = string.Join(",", StationPresets.Select(i => i.ToString(CultureInfo.InvariantCulture)).ToList());
+        return $"HasTweeters: {HasTweeters} HasSubWoofers: {HasSubWoofers} Station Presets: {presets} Radio ID: {RadioId}";
+    }
+}
---
+++ ProSharp/Serializations/View.cs
@@ -0,0 +1,14 @@
+﻿namespace ProSharp.Serializations;
+
+public class View
+{
+    public void Showcase()
+    {
+        Console.WriteLine("========> Fun With Object Serialization\n");
+        
+        var myCars = ObjectFactory.GetJamesBondCars();
+        var person = ObjectFactory.GetPerson("James", true);
+        
+        
+    }
+}
